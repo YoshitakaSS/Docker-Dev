e7f69510b14bd18641496c735e776c359a6c20cf
ys <yoshi.23unog34@gmail.com>
2020-04-05 21:34:39 +0900

    @add laravel debugar settings

--- docker/php/local_setting/init.sh
+++ docker/php/local_setting/init.sh
@@ -1,4 +1,5 @@
 php artisan key:generate
+php artisan vendor:publish --provider="Barryvdh\Debugbar\ServiceProvider"
 
 chmod -R 777 /var/www/dev-web/bootstrap/cache
 chmod -R 777 /var/www/dev-web/storage
